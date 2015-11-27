d445b0741540973008faf1a695bb5c7fecdc6008
Jonathon <jonathon.nordquist@gmail.com>
2015-11-27 22:12:47 +0000

    Create list model and migration.  Add validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :lists, dependent: :destroy
+  
   validates :name, presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
