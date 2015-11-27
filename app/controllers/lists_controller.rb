0221a706d93728bbd02dc201acb927df106250ea
Jonathon <jonathon.nordquist@gmail.com>
2015-11-27 22:48:59 +0000

    Add create method for new list.  Add flash to display create results to _list_view partial.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,18 @@
 class ListsController < ApplicationController
+  def create
+    list = current_user.lists.new(list_params)
+    if list.save
+      flash.now[:success] = "Your new list has been created"
+    else
+      flash.now[:danger] = list.errors.full_messages[0]
+    end
+    
+    @lists = current_user.lists.all
+    render 'general_pages/homepage'
+  end
+  
+  private
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
