eb6de26eb940723e179089124bf69d4c53d870aa
Jonathon <jonathon.nordquist@gmail.com>
2015-11-27 18:47:59 +0000

    Add flash output to new session page.  Add session helpers.  Add create method to sessions controller.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+  def signin(user)
+    session[:user_id] = user.id
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def signed_in?
+    return !session[:user_id].nil?
+  end
+  
 end
