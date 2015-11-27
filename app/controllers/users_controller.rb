5570503adb6c5614e9f7f8037db10d4b6b343e2d
Jonathon <jonathon.nordquist@gmail.com>
2015-11-27 17:40:38 +0000

    Add show user view.  Add display of user info and flash message on account creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = "Your account has been created!"
+      redirect_to @user
     else
       render 'new'
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
