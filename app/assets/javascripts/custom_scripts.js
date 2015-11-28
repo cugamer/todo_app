b3545222a81dda1e2cdcc342efc8628e26be8f5d
Jonathon <jonathon.nordquist@gmail.com>
2015-11-28 20:24:45 +0000

    Integrate jquery-turbolinks gem to force proper loading of js when page is first opened.

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -1,5 +1,6 @@
 $(function () {
   $('.toggle').click(function (event) {
+    console.log("running")
   	event.preventDefault();
   	var target = $(this).attr('href');
   	$(target).toggleClass('hidden show');
