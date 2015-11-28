ffb2328d376c0253d6edebb66312aac6e4ff6428
Jonathon <jonathon.nordquist@gmail.com>
2015-11-28 19:28:42 +0000

    Create format_title helper method. User provide to add custom titles to each page.

--- app/helpers/general_pages_helper.rb
+++ app/helpers/general_pages_helper.rb
@@ -1,2 +1,10 @@
 module GeneralPagesHelper
+  def format_title(title = "")
+    base_title = "ToDo Application"
+    if(title.empty?)
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
 end
