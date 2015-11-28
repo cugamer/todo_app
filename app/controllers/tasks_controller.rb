3fa77cddec586571b14d03a1d31176ded3085f13
Jonathon <jonathon.nordquist@gmail.com>
2015-11-28 01:05:44 +0000

    Add basic display of tasks to tasks index page.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,22 @@ class TasksController < ApplicationController
     
     render 'index'
   end
+  
+  def create
+    list = List.find(task_params[:list_id])
+    task = list.tasks.new(task_params)
+    
+    if task.save
+      flash[:success] = "New task was created"
+    else
+      flash[:danger] = task.errors.full_messages[0]
+    end
+    
+    redirect_to show_tasks_path(list.id)
+  end
+  
+  private
+    def task_params
+      params.require(:task).permit(:name, :list_id)
+    end
 end
