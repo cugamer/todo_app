5a1b5f9caef31b45ed9703ed0a4d0506b1891072
Jonathon <jonathon.nordquist@gmail.com>
2016-04-01 17:17:57 +0000

    Add created_at ordering to task and list querys

--- app/controllers/general_pages_controller.rb
+++ app/controllers/general_pages_controller.rb
@@ -1,7 +1,7 @@
 class GeneralPagesController < ApplicationController
   def homepage
     if signed_in?
-      @lists = current_user.lists.all.paginate(:page => params[:page], :per_page => 10)
+      @lists = current_user.lists.all.order(:created_at).paginate(:page => params[:page], :per_page => 10)
     end
   end
   
